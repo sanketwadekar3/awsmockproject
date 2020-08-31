21eb773f88c0ff0f90ce21ddd2d65af91ba5512f
akshaygupta213 <69839930+akshaygupta213@users.noreply.github.com>
2020-08-31 12:23:49 +0530

    Add files via upload

---
+++ modules/api_get_method/var.tf
@@ -0,0 +1,39 @@
+
+variable "rest_api_id" {
+
+}
+variable "rest_api_id2" {
+
+}
+variable "rest_api_id3" {
+
+}
+variable "rest_api_id4" {
+
+}
+variable "rest_api_id5" {
+
+}
+
+variable "parent_id" {
+
+}
+variable "path_part" {
+
+}
+variable "http_method" {
+ default = "GET"
+}
+/*
+variable "authorization" {
+  default = "COGNITO_USER_POOLS"
+}
+variable "auth_id" {
+  
+}*/
+variable "type" {
+    default = "AWS"
+}
+variable "uri" {
+    
+}
